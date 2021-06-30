455aa49b9f0130fbc6dd627fd14f12e5141f82ba
Stephen Odundo <steveremdi@gmail.com>
2021-06-30 11:29:19 +0300

    Add project files.

---
+++ Fusion2CSV/Fusion2CSV.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fusion2CSV
+{
+    public class Fusion2CSV
+    {
+        public void ResponseToFile(string response, string file)
+        {
+            var csvText = ResponseFormatter(response);
+            WriteToCSV(csvText, file);
+        }
+
+        private string ResponseFormatter(string response)
+        {
+            var newResponse = response.Replace(@"\n", "\n");
+            newResponse = newResponse.Replace(",", ", ");
+            newResponse = newResponse.Replace("\"", "");
+
+            return newResponse;
+        }
+
+        private void WriteToCSV(string response, string file)
+        {
+            System.IO.File.WriteAllText(file, response);
+        }
+    }
+}
